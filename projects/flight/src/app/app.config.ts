ccf9ec49ac930c134637f5261526e61b7c291e2e
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-03 13:53:05 +0200

    refactor: fn http interceptors

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withDebugTracing } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { APP_ROUTES } from './app.routes';
+import { AuthInterceptor, authInterceptor } from './shared/logic-communication/auth/auth.interceptor';
 import { provideRouterFeature } from './shared/logic-router-state';
 
 export const appConfig: ApplicationConfig = {
@@ -13,7 +14,17 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
       // withDebugTracing()
     ),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor
+      ])
+      // withInterceptorsFromDi()
+    ),
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   multi: true,
+    //   useClass: AuthInterceptor
+    // },
     provideStore(),
     provideEffects(),
     provideRouterFeature(),
--- projects/flight/src/app/shared/logic-communication/auth/auth.interceptor.ts
+++ projects/flight/src/app/shared/logic-communication/auth/auth.interceptor.ts
@@ -3,6 +3,25 @@ import { Injectable } from "@angular/core";
 import { Observable, tap } from "rxjs";
 
 
+export function authInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+  console.log('HTTP Request', req.method, req.url);
+
+  if (req.url.startsWith('https://demo.angulararchitects.io/api')) {
+    const headers = req.headers.set(
+      'Authorization',
+      'Bearer MyAuthToken-A1B2C3'
+    );
+    req = req.clone({ headers });
+  }
+
+  return next(req).pipe(
+    tap(resp => console.log('HTTP Response Log Info from Root', resp))
+  );
+}
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(
@@ -24,3 +43,4 @@ export class AuthInterceptor implements HttpInterceptor {
     );
   }
 }
+
