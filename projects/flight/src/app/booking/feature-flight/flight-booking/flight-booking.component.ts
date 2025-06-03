eadf96547745da39353f70ded31180b732d5340f
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-03 15:26:19 +0200

    demo: local providers on component level and provided through directive (shared injector node w/ component)

--- projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '../../../shared/ui-common';
+import { provideNavigationService } from '../../../shared/logic-navigation';
+import { NavigationProviderDirective } from '../../../shared/ui-common/navigation/navigation-provider.directive';
 
 
 @Component({
   selector: 'app-flight-booking',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavigationComponent,
+    NavigationProviderDirective
+  ],
+  providers: [
+    provideNavigationService([
+      {
+        route: 'flight/search',
+        label: 'Flight Search',
+        icon: 'flight'
+      },
+      
+    ])
   ],
   template: `
+    <app-navigation class="nav-standalone" />
+    <app-navigation [navConfig]="navState" class="nav-standalone" />
+
     <div>
       <router-outlet></router-outlet>
     </div>
   `
 })
-export class FlightBookingComponent {}
+export class FlightBookingComponent {
+  protected navState = [{
+    route: 'flight/departures',
+    label: 'Departures',
+    icon: 'departures'
+  }];
+}
