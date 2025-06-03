8b5b7fa0ee054ea5e79b08d116d674202fffe471
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-03 10:41:42 +0200

    refactor: standalone schematics generator - option 1

--- projects/flight/src/app/booking/booking.module.ts
+++ projects/flight/src/app/booking/booking.module.ts
@@ -1,27 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { BookingRoutingModule } from './booking-routing.module';
 import { TicketEffects } from './logic-flight/+state/effects';
 import { ticketFeature } from './logic-flight/+state/reducer';
-import { FlightBookingComponent, FlightEditComponent, FlightSearchComponent } from './feature-flight';
-import { ReactiveFormsModule } from '@angular/forms';
-import { UiFlightModule } from './ui-flight/ui-flight.module';
 
 
 @NgModule({
-  declarations: [
-    FlightBookingComponent,
-    FlightEditComponent,
-  ],
-  imports: [
-    CommonModule,
-    BookingRoutingModule,
-    ReactiveFormsModule,
-    StoreModule.forFeature(ticketFeature),
-    EffectsModule.forFeature([TicketEffects]),
-    UiFlightModule
-  ]
+    imports: [
+        BookingRoutingModule,
+        StoreModule.forFeature(ticketFeature),
+        EffectsModule.forFeature([TicketEffects]),
+    ]
 })
 export class BookingModule { }
