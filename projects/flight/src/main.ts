2504a431ac7b414f0ec1b47f81118ad7cc9c3af1
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-03 10:52:20 +0200

    refactor: standalone schematics generator - option 3 - bootstrapping

--- projects/flight/src/main.ts
+++ projects/flight/src/main.ts
@@ -1,6 +1,28 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { routerFeature } from './app/shared/logic-router-state';
+import { SharedModule } from './app/shared/shared.module';
+import { UiCoreModule } from './app/shared/ui-core/ui-core.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      StoreModule.forRoot(),
+      EffectsModule.forRoot(),
+      StoreModule.forFeature(routerFeature),
+      UiCoreModule,
+      SharedModule
+    )
+  ]
+})
   .catch(err => console.error(err));
