2be95b879703f9b69912cfdcf6cf8d79ecc6c55a
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-04 11:14:32 +0200

    fix: tidy-up

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -3,7 +3,6 @@ import { Component, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
-import { ReactiveNode, SIGNAL } from '@angular/core/primitives/signals';
 
 
 @Component({
@@ -34,10 +33,7 @@ export class FlightSearchComponent {
   protected flights$ = this.ticketsFacade.flights$;
 
   constructor() {
-    let activeConsumer: ReactiveNode | null;
-    activeConsumer = effect(() => console.log(this.route()));
-
-    console.log(this.route[SIGNAL]);
+    effect(() => console.log(this.route()));
   }
 
   protected search(filter: FlightFilter): void {
